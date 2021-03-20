74fb30b36bc1e2ac74a684f10125b3c11a0dd968
jlong <jie.long@killbuqs.de>
2021-03-21 00:40:12 +0100

    Added product category for query and delete and add

---
+++ common/renren-fast-vue/src/views/modules/product/category.vue
@@ -0,0 +1,141 @@
+<!--  -->
+<template>
+  <div>
+    <el-tree
+      :data="menus"
+      show-checkbox
+      :props="defaultProps"
+      :expand-on-click-node="false"
+      node-key="catId"
+      :default-expanded-keys="expandedKey"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="node.level <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)"
+          >
+            Append
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >
+            Delete
+          </el-button>
+        </span>
+      </span>
+    </el-tree>
+
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+      <el-form :model="category">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCategory">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+
+  data() {
+    return {
+      category: { name: "", parentCid: 0, catLevel: 0, showStatus: 1, sort: 0 },
+      dialogVisible: false,
+      menus: [],
+      expandedKey: [],
+      defaultProps: {
+        children: "children",
+        label: "name",
+      },
+    };
+  },
+  methods: {
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get",
+      }).then(({ data }) => {
+        this.menus = data.data;
+      });
+    },
+    append(data) {
+      console.log("append", data);
+      this.dialogVisible = true;
+      this.category.parentCid = data.catId;
+      this.category.catLevel = data.catLevel * 1 + 1;
+    },
+    //添加三级分类
+    addCategory() {
+      console.log("添加三级分类", this.category);
+      this.$http({
+        url: this.$http.adornUrl("/product/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.category, false),
+      }).then(({ data }) => {
+        this.$message({
+          type: "success",
+          message: "菜单保存成功!",
+        });
+        //关闭对话框
+        this.dialogVisible = false;
+        //刷新出新的菜单
+        this.getMenus();
+        //设置默认展开的菜单
+        console.log("parentCid: " + this.category.parentCid);
+        this.expandedKey = [this.category.parentCid];
+      });
+    },
+
+    remove(node, data) {
+      console.log("remove", node, data);
+      this.$confirm(`是否删除【${data.name}】菜单?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          var ids = [data.catId];
+          this.$http({
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(ids, false),
+          }).then(({ data }) => {
+            this.$message({
+              type: "success",
+              message: "删除成功!",
+            });
+            //刷新出新的菜单
+            this.getMenus();
+            //设置默认展开的菜单
+            this.expandedKey = [node.parent.data.catId];
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除",
+          });
+        });
+    },
+  },
+  created() {
+    this.getMenus();
+  },
+};
+</script>
+<style scoped>
+</style>
---
+++ common/vue2/Readme.md
@@ -0,0 +1,23 @@
+npm init -y
+
+npm install vue
+
+npm install -g vue-cli
+
+~/.nvm/versions/node/v14.15.1/bin/vue init webpack vue-demo
+
+Vue模块化开发
+1.全局安装webpack
+npm install webpack -g
+
+2.全局安装vue脚手架
+npm install -g @vue/cli-init
+
+3.初始化vue项目
+vue init webpack appname：vue脚手架使用webpack模板初始化一个appname项目
+
+4.启动vue项目
+npm start = npm run dev
+
+安装Element UI
+npm i element-ui
