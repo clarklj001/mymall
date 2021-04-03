99bbbd16150dd40cec9374165e0ede57e9a65169
jlong <jie.long@killbuqs.de>
2021-04-03 12:52:24 +0200

    Added attrgroup view

---
+++ common/renren-fast-vue/src/views/modules/common/category.vue
@@ -0,0 +1,58 @@
+<!--  -->
+<template>
+  <el-tree
+    :data="menus"
+    :props="defaultProps"
+    :expand-on-click-node="false"
+    node-key="catId"
+    ref="menuTree"
+    @node-click="nodeclick"
+  >
+  </el-tree>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      menus: [],
+      expandedKey: [],
+      defaultProps: {
+        children: "children",
+        label: "name",
+      },
+    };
+  },
+  computed: {},
+  watch: {},
+  methods: {
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get",
+      }).then(({ data }) => {
+        this.menus = data.data;
+      });
+    },
+    nodeclick(data, node, component) {
+      console.log("子组件的节点被点击", data, node, component);
+      this.$emit("tree-node-click", data, node, component);
+    },
+  },
+  created() {
+    this.getMenus();
+  },
+  mounted() {},
+  beforeCreate() {},
+  beforeMount() {},
+  beforeUpdate() {},
+  update() {},
+  beforeDestroy() {},
+  destroyed() {},
+  activated() {}, // for keep-alive cache function
+};
+</script>
+<style scoped>
+</style>
