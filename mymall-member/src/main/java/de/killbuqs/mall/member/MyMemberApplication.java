12d3d18c9aebca658ac495f9301d8f35863fbb95
jlong <jie.long@killbuqs.de>
2021-04-04 16:22:34 +0200

    Register member module to gateway router

--- mymall-member/src/main/java/de/killbuqs/mall/member/MyMemberApplication.java
+++ mymall-member/src/main/java/de/killbuqs/mall/member/MyMemberApplication.java
@@ -2,11 +2,13 @@ package de.killbuqs.mall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "de.killbuqs.mall.member.feign")
 public class MyMemberApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(MyMemberApplication.class, args);
