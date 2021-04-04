c14b3a07119507709cf44be97e54c2f4c908d86d
jlong <jie.long@killbuqs.de>
2021-04-04 16:20:24 +0200

    Updated sql script for table pms_attr

--- common/mysql/sql/mymall_pms.sql
+++ common/mysql/sql/mymall_pms.sql
@@ -39,6 +39,7 @@ create table pms_attr
    icon                 varchar(255) comment '属性图标',
    value_select         char(255) comment '可选值列表[用逗号分隔]',
    attr_type            tinyint comment '属性类型[0-销售属性，1-基本属性，2-既是销售属性又是基本属性]',
+   value_type           tinyint comment '值类型',
    enable               bigint comment '启用状态[0 - 禁用，1 - 启用]',
    catelog_id           bigint comment '所属分类',
    show_desc            tinyint comment '快速展示【是否展示在介绍上；0-否 1-是】，在sku中仍然可以调整',
